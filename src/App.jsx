d6c5561ded459cd9a7e8c868e6d91b32c27dd43e
Josh Tummel <joshtummel@Joshs-MacBook-Air.local>
2019-05-14 22:25:58 -0400

    updating deps

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,10 @@
-import React, { useReducer } from 'react';
-import './App.css';
-
-import { initialState, reducer } from './form/reducers';
-import actions from './form/actions';
+import React from 'react';
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const { firstCount, secondCount } = state;
-
-  const handleButtonAddClick = () => {
-    dispatch(actions.increment(5));
-  };
-
-  const handleButtonSubtractClick = () => {
-    dispatch(actions.decrement(10));
-  };
-  // Use the Consumer to grab the value from context
-  // Notice this component didn't get any props!
   return (
     <>
-      <p>
-        total is {firstCount} {secondCount}
-      </p>
-      <button type="button" onClick={handleButtonAddClick}>
-        Add
-      </button>
-      <button type="button" onClick={handleButtonSubtractClick}>
-        Subtract
-      </button>
+      <h1>My app</h1>
+      <p>Testing styles</p>
     </>
   );
 };
