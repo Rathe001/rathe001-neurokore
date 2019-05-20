f49d92565f2057bb40152e75ab9cec1577378178
Josh Tummel <joshtummel@Joshs-MacBook-Air.local>
2019-05-19 21:05:21 -0400

    rework ui

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -4,15 +4,16 @@ import { connect } from 'react-redux';
 import withStyles from 'react-jss';
 import classnames from 'classnames';
 import uiActions from 'core/ui/actions';
+import splashBg from 'assets/img/bg-splash.png';
 
 const styles = {
   splash: {
     cursor: 'pointer',
     display: 'flex',
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
     color: 'red',
-    background: `#333`,
     position: 'absolute',
     top: 0,
     left: 0,
@@ -21,11 +22,29 @@ const styles = {
     zIndex: 9999,
     opacity: 1,
     transition: '1s all',
+    background: `url(${splashBg}) 0 0 no-repeat`,
+    backgroundSize: 'auto 100%',
   },
   hidden: {
     opacity: 0,
     zIndex: -1,
     transform: 'scale(1.3)',
+  },
+  logo: {
+    fontSize: 24,
+  },
+  start: {
+    textShadow: '0 0 6px rgba(0, 0, 0, 0.9)',
+    opacity: 1,
+    animation: 'flickerAnimation 1s infinite',
+    fontSize: 16,
+    display: 'block',
+  },
+
+  '@keyframes flickerAnimation': {
+    '0%': { opacity: 1 },
+    '50%': { opacity: 0 },
+    '100%': { opacity: 1 },
   },
 };
 
@@ -37,7 +56,8 @@ const Splash = ({ classes, toggleSplash, showSplash }) => {
         [classes.hidden]: !showSplash,
       })}
     >
-      Click to start
+      <span className={classes.logo}>[Insert Title Here]</span>
+      <span className={classes.start}>Click to start</span>
     </div>
   );
 };
