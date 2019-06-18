7a0c99ff1dc0c927615dd934d0476d0364dcdd1b
Josh Tummel <josh@astigmapro.com>
2019-06-18 11:19:32 -0400

    updates

--- src/constants/styles.js
+++ src/constants/styles.js
@@ -11,6 +11,9 @@ export const JSS_RESET = {
   '*, *:before, *:after': {
     boxSizing: 'inherit',
   },
+  body: {
+    overflow: 'hidden',
+  },
   'body, h1, h2, h3, h4, h5, h6, p, ol, ul': {
     margin: 0,
     padding: 0,
