d98096d197f1f6df4795fcdbe103d286963f72a0
Josh Tummel <jtummel@bottomline.com>
2021-04-21 09:48:51 -0400

    Fix paths

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export function fetchLevel(levelId) {
-  return axios.get(`/api/levels/${levelId}.json`);
+  return axios.get(`/neurokore/api/levels/${levelId}.json`);
 }
 
 export default fetchLevel;
